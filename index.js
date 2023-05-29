2f6b52f4aeebd85bc1af77f8a024612991e86644
Karolis <karolisdarbinis2@gmail.com>
2023-05-29 15:06:14 +0300

    Numbers

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+
+Esminiai konceptai programavime:
+
+1) kintamieji - info atsiminimas
+2) if'ai - info palyginimas
+3) loop'ai - opreciju vykdymas n kartu
+4) functions - pasikartojancios logikos aprasymas
+
+
+
+*/
--- js/02-numbers.js
+++ js/02-numbers.js
@@ -10,6 +10,15 @@ Begalybes ir NaN (not a number)
 Aritmetiniai operatoriai: 
 +, -, *, /
 
+Salia matematikos:
+(, )
+
+Kintamuju iniciavimo budai:
+
+const - default
+let - naudoti, tik jei reikia keisti reiksmes
+var - senove, todel niekada nenaudoti, nebent TIKRAI zinai ka darai
+
 */
 
 console.log(1);
@@ -30,3 +39,47 @@ console.log(NaN);
 console.log(2+2);
 console.log(2 + 2 * 2);
 
+console.log(2 + 2, 4);
+console.log(2 + 2 * 2, 6);
+
+console.log(0.1 + 0.2, 0.3);
+
+console.clear();
+
+// KINTAMIEJI
+
+const amzius = 20; //priskiriam, nekinta
+console.log(amzius);
+
+const saldainiuKiekis = 19;
+const saldainiu_Kiekis = 29;
+
+console.log(saldainiuKiekis);
+
+console.log(saldainiu_Kiekis);
+
+console.clear();
+
+let pinigine = 0;
+console.log(pinigine);
+console.log(pinigine);
+console.log(pinigine);
+console.log(pinigine);
+
+pinigine = 20;
+console.log(pinigine);
+console.log(pinigine);
+console.log(pinigine);
+console.log(pinigine);
+
+pinigine = 5 + 5;
+console.log(pinigine);
+
+pinigine = pinigine + 3;
+console.log(pinigine);
+
+pinigine = pinigine * 3;
+console.log(pinigine);
+
+
+// Kintamieji: konstantos ir kintamieji
