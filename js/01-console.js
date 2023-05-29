d97f0a60f4d0c125f8bc3f93d86f916dc015ad69
Karolis <karolisdarbinis2@gmail.com>
2023-05-29 14:36:15 +0300

    Numbers, console, js

---
+++ js/01-console.js
@@ -0,0 +1,23 @@
+console.clear();
+
+console.log(1);
+console.log(2);
+console.log(2);
+
+// Single-line comment: ctrl + /
+
+/* Single-line as multi-line comment */
+
+/* Single-line
+as
+multi-line
+comment */
+
+/*
+    Single-line
+    as
+    multi-line
+    comment
+*/
+
+console.log(4);
---
+++ js/02-numbers.js
@@ -0,0 +1,32 @@
+console.clear();
+
+/*
+NUMBERS
+
+Sveikieji/desimtainiai
+Teigiami/neigiami
+Begalybes ir NaN (not a number)
+
+Aritmetiniai operatoriai: 
++, -, *, /
+
+*/
+
+console.log(1);
+console.log(2);
+console.log(3.14);
+console.log(2.72);
+
+console.log(-1);
+console.log(-2);
+console.log(-3.14);
+console.log(-2.72);
+
+console.log(Infinity);
+console.log(-Infinity);
+
+console.log(NaN);
+
+console.log(2+2);
+console.log(2 + 2 * 2);
+
