780075d8daf43cc89ac81eb04fe9a1ba9968dbd6
Karolis <karolisdarbinis2@gmail.com>
2023-06-01 18:28:25 +0300

    Training

--- js/07-array.js
+++ js/07-array.js
@@ -1,8 +1,8 @@
 console.clear();
 
-function suma(masyvas) {
-    return masyvas[0] + masyvas[1] + masyvas[2];
-}
+// function suma(masyvas) {
+//     return masyvas[0] + masyvas[1] + masyvas[2];
+// }
 
 const a1 = [1, 2, 3];
 const s1 = suma(a1);
@@ -17,5 +17,43 @@ console.log(s1);
 console.log(s2);
 console.log(s3);
 
+function suma(masyvas){
+    return masyvas[0] + masyvas[1] + masyvas[2]; 
+}
+
+ const b1 = [5, 5, 5];
+ const b2 = suma(b1);
+
+//console.log(suma([5, 5, 5]));
+
+console.log(b2);
 
 
+// Jonas: paskutinis pazymys yra 8.
+// Maryte: paskutinis pazymys yra 8.
+// Petras: paskutinis pazymys yra 7.
+// Ona: paskutinis pazymys yra 5.
+
+const stud1 = 'Jonas';
+const marks1 = [10, 2, 8, 2, 10];
+const stud2 = 'Maryte';
+const marks2 = [10];
+const stud3 = 'Juozas';
+const marks3 = [10, 2, 8, 2];
+const stud4 = 'Petras';
+const marks4 = [10, 2, 8];
+
+const index = marks1.length - 1;
+const index1 = marks2.length - 1;
+const index2 = marks3.length - 1;
+const index3 = marks4.length - 1;
+
+function studentIntro (studentas, pazymys){
+
+    return `${studentas}: paskutinis pazymys yra: ${pazymys}`;
+}
+
+console.log(studentIntro(stud1,marks1[index]));
+console.log(studentIntro(stud2,marks2[index1]));
+console.log(studentIntro(stud3,marks3[index2]));
+console.log(studentIntro(stud4,marks4[index3]));
