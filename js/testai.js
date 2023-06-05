da9a96edf7332a8b22db632c9a25b95350e77805
Karolis <karolisdarbinis2@gmail.com>
2023-06-05 15:29:26 +0300

    Ifs

--- js/testai.js
+++ js/testai.js
@@ -1,59 +1,86 @@
 console.clear();
 
 
-const empty = '     gg      ';
-console.log(empty.length);
+// const empty = '     gg      ';
+// console.log(empty.length);
 //                     11111
 //           012345678901234
-const str = 'Pavyzdys';
-console.log(str, 8, '>>>', str.length);
-console.log(str, 8, '>>>', str.length);
+// const str = 'Pavyzdys';
+// console.log(str, 8, '>>>', str.length);
+// console.log(str, 8, '>>>', str.length);
 
-const raide1 = str[0];
-console.log(raide1, 'P');
-console.log('---------------');
+// const raide1 = str[0];
+// console.log(raide1, 'P');
+// console.log('---------------');
 
-let strIndex = 0;
+// let strIndex = 0;
 
-console.log(str[strIndex++]);
-console.log(str[strIndex++]);
-console.log(str[strIndex++]);
-console.log(str[strIndex++]);
-console.log(str[strIndex++]);
-console.log(str[strIndex++]);
-console.log(str[strIndex++]);
-console.log(str[strIndex++]);
+// console.log(str[strIndex++]);
+// console.log(str[strIndex++]);
+// console.log(str[strIndex++]);
+// console.log(str[strIndex++]);
+// console.log(str[strIndex++]);
+// console.log(str[strIndex++]);
+// console.log(str[strIndex++]);
+// console.log(str[strIndex++]);
 
-console.log(str[strIndex++]);
-console.log(str[strIndex++]);
-console.log(str[strIndex++]);
+// console.log(str[strIndex++]);
+// console.log(str[strIndex++]);
+// console.log(str[strIndex++]);
 
-console.log(str[-1]);
-console.log(str[-2]);
-console.log(str[-24562]);
+// console.log(str[-1]);
+// console.log(str[-2]);
+// console.log(str[-24562]);
 
 
 
 // const userFirstName = 'Chuck';
 // const userLastName = 'Norris';
-const userFirstName = 'Pamela';
-const userLastName = 'Anderson';
+// const userFirstName = 'Pamela';
+// const userLastName = 'Anderson';
 
-const userAbbr = `${userFirstName[0]}.${userLastName[0]}.`;
-console.log(userAbbr);
+// const userAbbr = `${userFirstName[0]}.${userLastName[0]}.`;
+// console.log(userAbbr);
 
-const vardas = 'Karolis';
-console.log(vardas, 7, '>>>>', vardas.length);
-const raide = vardas[2];
-console.log(raide);
+// const vardas = 'Karolis';
+// console.log(vardas, 7, '>>>>', vardas.length);
+// const raide = vardas[2];
+// console.log(raide);
 
 
-console.clear();
+// console.clear();
 
-let kar = 8;
+// let kar = 8;
 
-let zaz;
+// let zaz;
 
-zaz = kar;
+// zaz = kar;
 
-console.log(zaz);
+// console.log(zaz);
+
+
+
+// function arrayPlusArray(arr1, arr2) {
+//     let sum = 0;
+//     let arr3 = [];
+//     arr3 = arr1.concat(arr2);
+//     for(let i = 0; i < arr3.length; i++){
+//         sum += arr3[i]; 
+//     }
+//     return sum;
+//   }
+
+//   let arr = [5, 7, 8, 9];
+//   let arr0 = [5, 10, 8, 1];
+//   console.log(arrayPlusArray(arr, arr0));
+
+// const day = 1;
+
+// const weekday = 'Not sure...';
+
+// if( day === 2){
+//     console.log(`Dabar pirmadienis`);
+// }else {
+//     console.log('Nezinau');
+// }
+
